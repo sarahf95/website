fb8dee879e04ac9b10683c5b2db67beb128c205f
Sarah Feldmann <felds@amazon.com>
2020-10-11 22:22:09 -0700

    Add landing page

---
+++ src/pageContainer/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './LandingPage.scss';
+
+function LandingPage() {
+
+    return (
+        <div className='pageContainer'>
+            <div className='titleText'>
+                <div className='title'>
+                    Sarah Feldmann
+                </div>
+                Full-stack software engineer in Seattle
+            </div>
+        </div>
+    );
+}
+export default LandingPage;
---
+++ src/pageContainer/Page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage'
+const styles = require('./Page.scss');
+
+function Page() {
+    return (
+        <div className='appPageContainer'>
+            <LandingPage />
+        </div>
+    );
+}
+export default Page;
